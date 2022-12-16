7354ba3318eb7741d5b625cf351d0556de30e5cc
Danil_Bespalov <dbaservice@yandex.ru>
2022-12-16 14:25:50 +0300

    //////

--- src/main/java/CarService/CarServiceImpl.java
+++ src/main/java/CarService/CarServiceImpl.java
@@ -9,15 +9,15 @@ import java.util.List;
 
 @Component
 public class CarServiceImpl implements CarService {
-
+    private int count;
     List<Car> carList = new ArrayList<>();
 
     {
-        carList.add(1, new Car("Model1", "driverName1", 1));
-        carList.add(2, new Car("Model2", "driverName2", 2));
-        carList.add(3, new Car("Model3", "driverName3", 3));
-        carList.add(4, new Car("Model4", "driverName4", 4));
-        carList.add(5, new Car("Model5", "driverName5", 5));
+        carList.add(1, new Car("Model1", "driverName1", ++count));
+        carList.add(2, new Car("Model2", "driverName2", ++count));
+        carList.add(3, new Car("Model3", "driverName3", ++count));
+        carList.add(4, new Car("Model4", "driverName4", ++count));
+        carList.add(5, new Car("Model5", "driverName5", ++count));
 
     }
     @Override
