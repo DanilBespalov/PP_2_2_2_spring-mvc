b1ce25866d9127b45ed02b27601888d5061e0656
Danil_Bespalov <dbaservice@yandex.ru>
2022-12-16 14:22:23 +0300

    //////

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,24 +3,23 @@ package web.controller;
 import CarService.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("/cars")
 public class CarController {
 
     @Autowired
     private CarService serviceCar;
     @GetMapping("/all")
-    public String printAllCars(Model model) {
+    public String printAllCars(ModelMap model) {
         model.addAttribute("cars", serviceCar.getCarList());
         return "cars/cars";
     }
 
     @GetMapping("/{index}")
-    public String showCarByID(@PathVariable("index") int index, Model model) {
+    public String showCarByID(@PathVariable("index") int index, ModelMap model) {
         model.addAttribute("car", serviceCar.getCarListByIndex(index));
         return "cars/car_index";
     }
